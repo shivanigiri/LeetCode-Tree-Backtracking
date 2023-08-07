ba2fd3c796004e8d6748924e25151fb44b3ad91c
Shivani Giri <42980529+shivanigiri@users.noreply.github.com>
2023-08-08 01:01:57 +0530

    Time: 0 ms (100%), Space: 40.3 MB (93.01%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -23,7 +23,7 @@ class Solution {
     }
     public boolean isSame(TreeNode p, TreeNode q){
         if(p==null && q== null) return true;
-        if((p!=null && q==null) || (p==null && q!=null)) return false;
+        if(p==null || q== null) return false;
         if(p.val!=q.val){
             return false;
         }
