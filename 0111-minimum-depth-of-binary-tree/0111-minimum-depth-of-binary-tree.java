a222716841c52d662340ae18191f53457acaefe4
Shivani Giri <42980529+shivanigiri@users.noreply.github.com>
2023-08-09 14:43:28 +0530

    Time: 5 ms (72.97%), Space: 61.6 MB (80.19%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -17,6 +17,7 @@ class Solution {
     public int minDepth(TreeNode root) {
         if(root==null) return 0;
         
+        //in case of skewed Tree
         if(root.left==null) return minDepth(root.right)+1;
         if(root.right==null) return minDepth(root.left)+1;
         
