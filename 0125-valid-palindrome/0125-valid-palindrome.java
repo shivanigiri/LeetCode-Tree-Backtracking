2ae573fd7add8e5de334acf6c8b0fb2099d4169e
Shivani Giri <42980529+shivanigiri@users.noreply.github.com>
2023-10-13 00:49:03 +0530

    Time: 9 ms (45.17%), Space: 51.4 MB (6.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,8 +2,7 @@ class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<s.length();i++){
-            if((s.charAt(i)>='a' && s.charAt(i)<='z')|| (s.charAt(i)>='A' && s.charAt(i)<='Z')
-            || (s.charAt(i)>='0' && s.charAt(i)<='9')){
+            if(Character.isLetterOrDigit(s.charAt(i))){
                 sb.append(s.charAt(i));
             }
         }
