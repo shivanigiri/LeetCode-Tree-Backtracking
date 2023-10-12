800dc856c2510dc73180b9781e62272962c90e16
Shivani Giri <42980529+shivanigiri@users.noreply.github.com>
2023-10-13 01:13:27 +0530

    Time: 2 ms (40.38%), Space: 43.9 MB (33.6%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,27 +1,29 @@
 class Solution {
+
     List<List<Integer>> res= new ArrayList<>();
+
     public List<List<Integer>> permute(int[] nums) {
-       // Map<Integer, Integer> freqMap=createFreqMap(nums);
-        //Arrays.sort(nums);
-        create(nums, new ArrayList<>(),new HashSet<>());
+       // Map<Integer, Integer> freqMap=createFreqMap(nums); 
+        generate(nums, new ArrayList<>(),new HashSet<>());
         return res;
     }
-    public void create(int[] nums,ArrayList<Integer> curr,HashSet<Integer> set){
+    public void generate(int[] nums,ArrayList<Integer> curr,HashSet<Integer> set){
          if(curr.size()==nums.length){
             res.add(new ArrayList<>(curr));
             return;
         }
-        for(int i=0;i<nums.length;i++){
-           // nelow line can be replaced with map as well
-           //if(curr.contains(nums[i])) continue;
-           if(set.contains(nums[i])) continue; // element already exists, skip
+       for(int i=0;i<nums.length;i++){
+           if(set.contains(nums[i])){ //skip repeated element of nums
+               continue;
+           }
            //do
-            set.add(nums[i]);
-            curr.add(nums[i]);
-            create(nums,curr,set);
-            //undo
-            curr.remove(curr.size()-1);
-            set.remove(nums[i]);
-        }
+           set.add(nums[i]);
+           curr.add(nums[i]);
+           generate(nums,curr,set);
+
+           //undo
+           set.remove(nums[i]);
+           curr.remove(curr.size()-1);
+       }
     }
 }
