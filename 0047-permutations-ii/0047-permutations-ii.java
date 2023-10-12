0bc133b0ccac6f559d8b96ea5c3f8c7628c49318
Shivani Giri <42980529+shivanigiri@users.noreply.github.com>
2023-10-13 02:30:24 +0530

    Time: 73 ms (18.25%), Space: 44.7 MB (14.12%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -15,10 +15,11 @@ class Solution {
         for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
             Integer num = entry.getKey();
             Integer count = entry.getValue();
-            if (count == 1)
+            if (count == 0)
                 continue;
             // add this number into the current combination
             curr.add(num);
+            System.out.println(curr +"count ::"+count+"map::"+entry);
             freqMap.put(num, count - 1);
 
             // continue the exploration
@@ -27,12 +28,14 @@ class Solution {
             // revert the choice for the next exploration
             curr.remove(curr.size()-1);
             freqMap.put(num, count);
+            System.out.println(curr+"count"+count);
         }
     }
     public  Map<Integer, Integer> createFreqMap(int[] nums){
         Map<Integer, Integer> freqMap=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            freqMap.put(nums[i],freqMap.getOrDefault(nums[i],1)+1);
+            freqMap.put(nums[i],freqMap.getOrDefault(nums[i],0)+1);
+            System.out.println(nums[i] +"count ::"+ freqMap.getOrDefault(nums[i],0)+1);
         }
         return freqMap;
     }
