ef57a8e61c0ae1d9f266c651866a9c1c6752c3ae
Shivani Giri <42980529+shivanigiri@users.noreply.github.com>
2023-10-12 23:58:54 +0530

    Time: 0 ms (100%), Space: 40.5 MB (75.36%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,36 +1,25 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(x==0) return 0;
-        return pow(x,(long)n);
-    }
-    public double pow(double x, long n) {
-        if(n<0){
-            n=(-1)*n;
-            x= (1.0)/x;
+        if(x==0){
+            return 0;
         }
-        double ans=1;
-        while(n != 0){
-            if(n%2 == 1){
-                ans= ans*x;
-                n=n-1;
-            }
-            n = n / 2;
-            x = x * x;
+        long pow=n; //taken long so that it can handhle Integer.MIN_VALUE
+        double ans=solve(x,Math.abs(pow));
+        if(pow<0){
+            return 1/ans;
         }
         return ans;
     }
-    // double ans=1;
-    //     if(n>0){
-    //         while(n>0){
-    //             ans*=x;
-    //             n--;
-    //         }
-    //     }else{
-    //         while(n<0){
-    //             ans*=x;
-    //             n++;
-    //         }
-    //         ans=1/ans;
-    //     }
-    //     return ans;
+    public double solve(double x, long n){
+        if(n==0){
+            return 1;
+        }
+        double temp= solve(x, n/2);
+        temp*=temp;
+        if(n%2==0){
+            return temp;
+        }else{
+            return temp*x;
+        }
+    }
 }
