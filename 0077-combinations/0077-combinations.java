8f0215b58db018754fd5098cda141a470dc4498f
Shivani Giri <42980529+shivanigiri@users.noreply.github.com>
2023-10-14 00:11:16 +0530

    Time: 7 ms (99.95%), Space: 91.9 MB (77.42%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,23 +1,31 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res= new ArrayList<>();
-        generate(res,n,k,0,new ArrayList<>());
+        generate(res,n,k,1,new ArrayList<>());
         return res;
     }
     public void generate(List<List<Integer>> res,int n,int size,int index,ArrayList<Integer> curr){
         if(size==0){
-            res.add(curr);
+            res.add(new ArrayList<>(curr));
             return ;
         }
-        if(index>=n){
-            return;
+        // if(index>=n){
+        //     return;
+        // }
+        // ArrayList<Integer> temp=new ArrayList<>(curr);
+        // //do
+        // temp.add(index+1);
+        // generate(res,n,size-1,index+1,temp);
+        // //undo
+        // generate(res,n,size,index+1,curr);
+        for(int i=index;i<=n-size+1;i++)  //index+1 coz i have started from 0
+        {
+            //do
+            curr.add(i);
+            generate(res,n,size-1,i+1,curr);
+            //undo
+            curr.remove(curr.size()-1);
         }
-        ArrayList<Integer> temp=new ArrayList<>(curr);
-        //do
-        temp.add(index+1);
-        generate(res,n,size-1,index+1,temp);
-        //undo
-        generate(res,n,size,index+1,curr);
-        
+
     }
 }
