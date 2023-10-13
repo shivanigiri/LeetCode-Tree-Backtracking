9cb368a10d765cc5a165410b8fb7b68ebb0332e2
Shivani Giri <42980529+shivanigiri@users.noreply.github.com>
2023-10-14 00:30:25 +0530

    Time: 10 ms (94.79%), Space: 92.2 MB (61.01%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -2,7 +2,6 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res= new ArrayList<>();
         generate(res,n,k,1,new ArrayList<>());
-        //generate(res,n,k,1,new ArrayList<>());
         return res;
     }
     public void generate(List<List<Integer>> res,int n,int size,int index,ArrayList<Integer> curr){
@@ -10,22 +9,23 @@ class Solution {
             res.add(new ArrayList<>(curr));
             return ;
         }
-        if(index>n){
-            return;
-        }
-        ArrayList<Integer> temp=new ArrayList<>(curr);
-        //do
-        temp.add(index);
-        generate(res,n,size-1,index+1,temp);
-        //undo
-        generate(res,n,size,index+1,curr);
-        // for(int i=index;i<=n-size+1;i++)  //index+1 coz i have started from 0
-        // {
-        //     //do
-        //     curr.add(i);
-        //     generate(res,n,size-1,i+1,curr);
-        //     //undo
-        //     curr.remove(curr.size()-1);
+        // if(index>n){
+        //     return;
         // }
+        // ArrayList<Integer> temp=new ArrayList<>(curr);
+        // //do
+        // temp.add(index);
+        // generate(res,n,size-1,index+1,temp);
+        // //undo
+        // generate(res,n,size,index+1,curr);
+
+        for(int i=index;i<=n-size+1;i++)  //index+1 coz i have started from 0
+        {
+            //do
+            curr.add(i);
+            generate(res,n,size-1,i+1,curr);
+            //undo
+            curr.remove(curr.size()-1);
+        }
     }
 }
