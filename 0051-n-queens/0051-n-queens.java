fde95300d5ffa4ad4463c30f87119aeb1cc1432d
Shivani Giri <42980529+shivanigiri@users.noreply.github.com>
2023-10-14 22:33:47 +0530

    Time: 18 ms (5.31%), Space: 44 MB (37.38%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,34 +1,30 @@
 class Solution {
      List<List<String>> res= new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-      int[] col=new int[n];
+        int[] col=new int[n];
         Arrays.fill(col,-1);
-        nqueen(0,col,n);
+        nqueen(n,col,0);
         return res;
-	}
-    public void nqueen(int r,int[] col,int A){
-        if(r==A){
+    }
+    public void nqueen(int n, int[] col,int r){
+        if(r==n){
             res.add(modelSolution(col));
             return;
         }
-        for(int c=0;c<A;c++){
+        for(int c=0; c<n; c++){
             if(isValid(r,c,col)){
-                //do
-                col[r]= c;
-                nqueen(r+1,col,A);
-                //undo
+                col[r]=c;
+                nqueen(n,col,r+1);
                 col[r]=-1;
             }
         }
     }
-    public boolean isValid(int r,int c,int[] A){
+    public boolean isValid(int r, int c, int[] col){
         for(int i=0;i<r;i++){
-            //check for same column
-            if(c==A[i]){
+            if(c==col[i]){
                 return false;
             }
-            //check for diagonal
-            if((r-c==i-A[i] )|| (r+c==i+A[i])){
+            if(( r+c == i+col[i] )||( r-c == i-col[i] )){
                 return false;
             }
         }
@@ -42,6 +38,24 @@ class Solution {
                 if(col[i]==j) curr.append('Q');
                 else curr.append('.');
             }
+            System.out.println("curr:: "+curr.toString()+ "finish");
+            ans.add(curr.toString());
+        }
+        return ans;
+    }
+    public ArrayList<String> modelSolutionsss(int[] col){
+       ArrayList<String> ans = new ArrayList<>();
+        for(int i=0; i<col.length; i++) {
+            StringBuilder curr = new StringBuilder();
+            for(int j=0;j<col.length;j++){
+
+                if(col[i]==j){
+                    curr.append('Q');
+                }else{
+                    curr.append('.');
+                }
+            }
+            System.out.println("curr:: "+curr.toString()+ "finish");
             ans.add(curr.toString());
         }
         return ans;
